f459a7ba1774b2fd1ef338b05cdf328946457474
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-01 18:35:33 +0500

    imp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,9 +8,53 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isDark = true;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              SwitchListTile(
+                value: isDark,
+                onChanged: (value) {
+                  if (isDark == true) {
+                    isDark = false;
+                  } else if (isDark == false) {
+                    isDark = true;
+                  }
+                  setState(() {});
+                },
+                title: Text("Is Dark Mode On"),
+                tileColor: Colors.yellow,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  side: BorderSide(
+                    color: Colors.red,
+                    width: 1,
+                  ),
+                ),
+                activeTrackColor: Colors.red,
+                inactiveTrackColor: Colors.blue,
+                activeColor: Colors.cyan,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
+
+
+
+
+
+
+
+
+
